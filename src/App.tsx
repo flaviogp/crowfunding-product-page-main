46613810eeaa3ccb57f8ea21631758cda3fef4f3
FlavioGP <flavio.gpinheiro@hotmail.com>
2023-12-19 10:30:27 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-xl text-orange-700 ">Hello World!</h1>
+      <p className="text-3xl font-bold underline">oi mundo</p>
+    </>
+  )
+}
+
+export default App
