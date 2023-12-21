381b64c10cdd22e7b12b8873f9666c3e083cf529
FlavioGP <flavio.gpinheiro@hotmail.com>
2023-12-21 10:45:30 -0300

    feat: finish hamvburger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,43 @@
 import { useState } from 'react'
 import Brand from '../assets/images/logo.svg'
+import HamburguerMenu from '../assets/images/icon-hamburger.svg'
+import CloseMenu from '../assets/images//icon-close-menu.svg'
 const Header = () => {
     const [openMenu, setOpenMenu] = useState(false);
 
     const handleChange = () => {
         setOpenMenu(!openMenu)
     }
-  return (  
-    <header className="container relative w-full h-[50px] flex justify-between p-3">
-        <div className="relative">
-            <img src={Brand} alt="Logo" />
-        </div>
-        <label htmlFor="menu">
-            <input type="checkbox" name="menu" id="menu" onChange={()=> handleChange()} />
-        </label>
 
-        {
-            openMenu &&
-            <nav className='absolute top-[100%] left-[5%] flex flex-col gap-4 bg-white w-[90%] h-max rounded-md'>
-                <ul>
+    const ImageHamburguerMenu = <img src={HamburguerMenu} alt="hamburguer menu" />
+    const ImageCloseMenu = <img src={CloseMenu} alt="close hamburguer menu" />
+    const MobileMenu = ( 
+            <nav className='lg:hidden absolute top-[100%] right-[5%] flex flex-col gap-4 bg-white w-[90%] max-w-[350px] h-max rounded-md'>
+                    <ul>
                     <li className='p-4 border-b-2 font-semibold'>About</li>
                     <li className='p-4 border-b-2 font-semibold'>Discover</li>
                     <li className='p-4 font-semibold'>Get Started</li>
                 </ul>
             </nav>
-        }
+        )
+  return (  
+    <header className="container relative w-full h-[50px] flex justify-between p-3">
+        <div className="relative">
+            <img src={Brand} alt="Logo" />
+        </div>
+
+        <nav className='sm:flex lg:flex md:flex lg: items center justify-end hidden'>
+            <ul className='flex gap-4 mr-5 text-white font-semibold'>
+                <li>About</li>
+                <li>Discover</li>
+                <li>Get Started</li>
+            </ul>
+        </nav>
+
+        <button className='sm:hidden transition' onClick={handleChange}>
+            {openMenu ? ImageCloseMenu : ImageHamburguerMenu}
+        </button>
+        {openMenu && MobileMenu}
     </header>
   )
 }
