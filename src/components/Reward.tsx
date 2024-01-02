9dc4d80b9e494fefcae8ac7dd164fb2d15dc2ef8
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-01-02 11:22:18 -0300

    feat: reward component

---
+++ src/components/Reward.tsx
@@ -0,0 +1,45 @@
+interface RewardProps {
+    title: string
+    pledge: number
+    description:string
+    itemsLeft: number
+
+}
+
+const Reward = ({title, description, itemsLeft, pledge}: RewardProps) => {
+  return (
+    <div className="container relative flex flex-col items-left gap-6 p-5 pb-10 w-[90%] max-w-[400px] bg-white border rounded-md">
+        <div>
+            <h2 className="font-bold text-xl">{title}</h2>
+            <span className="text-moderateCyan font-medium">Pledge ${pledge} or more</span>
+        </div>
+        <p className="text-darkGray">{description}</p>
+
+        <div className="flex items-center gap-2">
+            <p className="font-bold text-4xl">{itemsLeft}</p>
+            <span className="text-darkGray">left</span>
+        </div>
+
+        {
+            itemsLeft !== 0 ?
+                <button
+                    className="py-4 px-10 bg-moderateCyan rounded-full w-max font-bold text-white"
+                >
+                    Select Reward
+                </button>
+            :
+                <button
+                    className="py-4 px-10 bg-darkCyan rounded-full w-max font-bold text-white"
+                >
+                    Out of Stock
+                </button>
+        }
+        {
+            itemsLeft === 0 &&
+                <div className="absolute bg-white bg-opacity-60 top-0 l-0 w-full h-full rounded-md"></div>
+        }
+    </div>
+  )
+}
+
+export default Reward
